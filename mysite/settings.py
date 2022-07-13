76620ba067228626873f04e057d5a7390d1b1e1d
victor <kinrinrakko1014@outlook.jp>
2022-07-13 09:56:31 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 多分Django4で変わった（import os とすれば動く）
-# STATIC_ROOT = BASE_DIR / 'db.sqlite3',
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 多分Django4で変わった（import os とすれば動く）-> バカ
+# STATIC_ROOT = BASE_DIR / 'db.sqlite3',  # これのせいでデプロイ時にCSSの適用がうまくいかなかった！！
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
